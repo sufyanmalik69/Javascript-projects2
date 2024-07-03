e987783f7240ec3598558de8637b2b7e1e028a0c
sufyanmaliksaifi@gmail.com <sufyanmaliksaifi@gmail.com>
2024-07-03 17:02:33 +0500

    it is an order.

---
+++ Javascript-projects/JavaScript Notes/Loops.js
@@ -0,0 +1,16 @@
+let x=1;
+do{
+    console.log("I am batman.")
+    x++;
+    
+}while(x<=10);
+let y = 1;
+while(y<=10){
+    console.log("I am Homelander.");
+    y++;
+}
+let z;
+for(z=1;z<=10;z++){
+    console.log("I am Soldier Boy.")
+}
+
