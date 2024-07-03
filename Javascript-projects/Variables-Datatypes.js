4b805a0872cdcb7f43c0e93a8239b740073ae28a
sufyanmaliksaifi@gmail.com <sufyanmaliksaifi@gmail.com>
2024-07-03 12:39:50 +0500

    ee

---
+++ Javascript-projects/Variables-Datatypes.js
@@ -0,0 +1,39 @@
+console.log("hello world");
+
+// Integers/Numbers
+x = 99;
+console.log(x);
+
+// String
+Name = "Sufyan Malik";
+console.log("My name is %s",Name);
+
+// Null
+Gf = null;
+console.log(Gf);
+
+
+// Undefined
+Future = undefined;
+console.log(Future);
+
+
+// Boolean
+x = true;
+y = false;
+console.log(x,y);
+
+
+/* VARIABLE RULES*/
+
+// Variable names are case-sensitive javascript distuishes between "A" and "a".
+
+// Only letters,numbers,underscore(_) and $ is allowed.
+
+// Only letters,underscore(_) and $ can be the first character.
+
+// Reserved cannot be variable names.
+
+
+
+/* */
