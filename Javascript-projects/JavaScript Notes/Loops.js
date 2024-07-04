63b0be64ececaec32a152b69ce5e7a8cde8e804b
sufyanmaliksaifi@gmail.com <sufyanmaliksaifi@gmail.com>
2024-07-04 14:25:57 +0500

    hello

--- Javascript-projects/JavaScript Notes/Loops.js
+++ Javascript-projects/JavaScript Notes/Loops.js
@@ -1,16 +1,75 @@
-let x=1;
-do{
-    console.log("I am batman.")
-    x++;
-    
-}while(x<=10);
-let y = 1;
-while(y<=10){
-    console.log("I am Homelander.");
-    y++;
-}
-let z;
-for(z=1;z<=10;z++){
-    console.log("I am Soldier Boy.")
-}
+// LOOPS
 
+//do-while loop
+
+// let x=1;
+// do{
+//     console.log("I am batman.")
+//     x++;
+    
+// }while(x<=10);
+
+
+// while loop
+// let y = 1;
+// while(y<=10){
+//     console.log("I am Homelander.");
+//     y++;
+// }
+
+// for loop
+// let z;
+// for(z=1;z<=10;z++){
+//     console.log("I am Soldier Boy.")
+// }
+
+// for-of loop
+// x = "Hello world!" // is used to iterate strings
+// for(let i of x){ // <--works as for loop in python for e.g. for i in x:
+//     console.log(i)
+// }
+
+
+// for-in loop 
+// student = {
+//     Name:"Sufyan malik",
+//     age : 18,
+//     City: "karachi",
+//     Status: true
+// }
+
+// for(let i in student)[ // this loop is used to iterate arrays and objects(dicts).
+//     console.log("key =",i,"\nvalue =",student[i])
+// ]
+
+
+// print odd between 1 to 100
+// let x = 1;
+// while(x<=100){
+//     if(x%2 === 1){
+//         console.log(x)
+//     }
+//     x++
+// }
+
+const prompt = require("prompt-sync")();
+
+// make number guesser game
+let number = 35;
+
+while(true){
+    let u_guess = prompt("Enter your guess: ");
+    if(u_guess == number){
+        console.log("Congratulations, You have guessed it right.")
+        break
+    }
+    if(u_guess < number){
+        console.log("A bit higher.")
+    }
+    if(u_guess > number){
+        console.log("A it lower.")
+    
+    }
+
+
+}
---
+++ html-css/script.js
@@ -0,0 +1,7 @@
+// practice Q1. Take an input asking user its full name and Generate a username with @ at he start
+// , users full name at the middle and and the length of his name at the end.s
+
+// var x = prompt("Please, enter your FullName")
+// x = "@" + x + String(x.length)
+// console.log("You're generated Username is %s",x)
+
